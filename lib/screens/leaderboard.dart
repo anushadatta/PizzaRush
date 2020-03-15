2842d9e38edbd1852b188629a4aa158dd8ce314f
Anusha Datta <anushadatta@gmail.com>
2020-03-16 00:51:58 +0800

    Cleanup

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -9,46 +9,21 @@ class Leaderboard extends StatefulWidget {
 class _LeaderboardState extends State<Leaderboard> {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      
-      home: DefaultTabController(
-        length: 3,
-        
-        child: Scaffold(
-          
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(85.0),
-              child: AppBar(
-              
-              backgroundColor: Colors.grey[50], 
-               
-              bottom: TabBar(
-                tabs: [
-                  Tab(child: Text('ALGEBRA', 
-                  style: TextStyle(
-                    color: Colors.blue[600], 
-                    fontWeight: FontWeight.bold),
-                  )),
-                 
-                  Tab(child: Text('GEOMETRY', 
-                  style: TextStyle(
-                    color: Colors.blue[600], 
-                    fontWeight: FontWeight.bold,),
-                  )),
-                 
-                  Tab(child: Text('TRIGO.', 
-                  style: TextStyle(
-                    color: Colors.blue[600], 
-                    fontWeight: FontWeight.bold),
-                  )),
-                ],
-              ),
-              
-              title: Text('LEADERBOARD',
-              style: TextStyle(fontSize: 35, color: Colors.black87, fontWeight: FontWeight.bold)),
-              centerTitle: true,
-              elevation: 0.0,
+    
+      return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('LEADERBOARD'),
+            centerTitle: true,
+            backgroundColor: Colors.green[800],
+            bottom: TabBar(
+              indicatorColor: Colors.green[800],
+              tabs: [
+                Tab(text: 'Algebra',),
+                Tab(text: 'Trigonometry',),
+                Tab(text: 'Geometry',),
+              ],
             ),
           ),
 
@@ -60,7 +35,6 @@ class _LeaderboardState extends State<Leaderboard> {
             ],
           ),
         ),
-      ),
     );
   }
 }
