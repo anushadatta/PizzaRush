ce2ada7347877c7e99eb4620e12536a01c440bb5
Divvij Chandna <divvijchandna@gmail.com>
2020-04-18 18:49:03 +0800

    Challenge

--- lib/screens/challenge.dart
+++ lib/screens/challenge.dart
@@ -11,9 +11,9 @@ class Challenge extends StatefulWidget {
 }
 
 class _ChallengeState extends State<Challenge> {
-  String dropdownValue1 = 'Algebra';
-  String dropdownValue2 = 'Easy';
-  String dropdownValue3 = 'palakasomani@gmail.com';
+  String _topicValue;
+  String _difficultyValue;
+  String _challengerValue;
 
 
   @override
@@ -84,39 +84,41 @@ class _ChallengeState extends State<Challenge> {
               ],
             ),
 
-
-
             Row(
               //mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
                 children: <Widget>[
-                  SizedBox(width: 150,),
-                  DropdownButton<String>(
-                    value: dropdownValue1,
-                    icon: Icon(Icons.arrow_drop_down,size: 50),
-                    iconSize: 30,
-                    elevation: 26,
-                    style: TextStyle(
-                      color: Colors.green[800],fontSize: 25,
+                  Container(
+                    width: 392.0,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+                    child: DropdownButton<String>(
+                      hint: Text('Topic'),
+                      style: TextStyle(
+                        color: Colors.green[800],fontSize: 25,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _topicValue = value;
+                        });
+                      },
+                      value: _topicValue,
+                      elevation: 16,
+                      items: <String>['Algebra', 'Geometry','Trigonometry']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value.toLowerCase(),
+                          child: Text(value, textAlign: TextAlign.center),
+                        );
+                      })
+                          .toList(),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.green[600],
+                      ),
                     ),
-                    underline: Container(
-                      height: 4,
-                      width: 12,
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue1 = newValue;
-                      });
-                    },
-                    items: <String>['Algebra', 'Geometry','Trigonometry']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value, textAlign: TextAlign.center),
-                      );
-                    })
-                        .toList(),
-
-                  ),]),
+                  )
+                ]
+            ),
 
             SizedBox(height: 20),
             Row(
@@ -129,76 +131,70 @@ class _ChallengeState extends State<Challenge> {
               ],
             ),
 
-
-
             Row(
               //mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
                 children: <Widget>[
-                  SizedBox(width: 160,),
-                  DropdownButton<String>(
-                    value: dropdownValue2,
-                    icon: Icon(Icons.arrow_drop_down,size: 50),
-                    iconSize: 30,
-                    elevation: 26,
-                    style: TextStyle(
-                      color: Colors.green[800],fontSize: 25,
+                  Container(
+                    width: 392.0,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+                    child: DropdownButton<String>(
+                      hint: Text('Difficulty'),
+                      style: TextStyle(
+                        color: Colors.green[800],fontSize: 25,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _difficultyValue = value;
+                        });
+                      },
+                      value: _difficultyValue,
+                      elevation: 16,
+                      items: <String>['Easy', 'Medium', 'Hard']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value.toLowerCase(),
+                          child: Text(value, textAlign: TextAlign.center),
+                        );
+                      })
+                          .toList(),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.green[600],
+                      ),
                     ),
-                    underline: Container(
-                      height: 4,
-                      width: 12,
-
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue2 = newValue;
-                      });
-                    },
-                    items: <String>['Easy', 'Medium', 'Hard']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    })
-                        .toList(),
-                  ),]),
+                  )
+                ]
+            ),
 
             SizedBox(height: 25),
 
-            Center(child: Text('Select User to Challenge',textAlign: TextAlign.center,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold))),
+            Center(child: Text('User to Challenge',textAlign: TextAlign.center,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold))),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-
-                  DropdownButton<String>(
-                    value: dropdownValue3,
-                    icon: Icon(Icons.arrow_drop_down,size: 50),
-                    iconSize: 30,
-                    elevation: 26,
-                    style: TextStyle(
-                      color: Colors.green[800],fontSize: 25,
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                    width: 230.0,
+                    height: 60.0,
+                    child: TextFormField(
+                      maxLines: 1,
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                          enabledBorder: new OutlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.green[800], width: 2.0)
+                          ),
+                          focusedBorder: new OutlineInputBorder(
+                              borderSide: new BorderSide(color: Colors.green[800], width: 2.0)
+                          ),
+                          hintText: 'User ID',
+                      ),
+                      onChanged: (val) {
+                        setState(() => _challengerValue = val);
+                      },
                     ),
-                    underline: Container(
-                      height: 4,
-                      width: 12,
-
-                    ),
-                    onChanged: (String newValue) {
-                      setState(() {
-                        dropdownValue3 = newValue;
-                      });
-                    },
-
-                    // EXTRACT USERS TO CHALLENGE FROM FIREBASE 
-                    items: <String>['palakasomani@gmail.com', '' ]
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    })
-                        .toList(),
                   ),
                 ]
               ),
@@ -213,7 +209,7 @@ class _ChallengeState extends State<Challenge> {
                         child: RaisedButton(
                           color: Colors.white70,
                           onPressed: (){
-                            Navigator.push(context, CupertinoPageRoute(builder: (context) => ChallengeQuestion(topic: dropdownValue1, level: dropdownValue2, challengee: dropdownValue3,)));
+                            Navigator.push(context, CupertinoPageRoute(builder: (context) => ChallengeQuestion(topic: _topicValue, level: _difficultyValue, challengee: _challengerValue,)));
                           },
 
                           child: Text(
--- lib/screens/challenge_question.dart
+++ lib/screens/challenge_question.dart
@@ -1,12 +1,15 @@
+import 'package:PizzaRush/models/question.dart';
 import 'package:PizzaRush/screens/performace.dart';
 import 'package:PizzaRush/screens/performace_challenger.dart';
+import 'package:PizzaRush/screens/user_account.dart';
+import 'package:PizzaRush/services/collections.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChallengeQuestion extends StatefulWidget {
-  var topic;
-  var level;
-  var challengee; 
+  final topic;
+  final level;
+  final challengee;
 
   ChallengeQuestion(
     {
@@ -22,11 +25,17 @@ class ChallengeQuestion extends StatefulWidget {
 
 }
 
-enum Answers{a,b,c,d} 
 
 class _ChallengeQuestionState extends State<ChallengeQuestion> {
-  Answers _character = Answers.a;
-  
+
+  Future<List> questions;
+  String _q1ans;
+  String _q2ans;
+  String _cans1;
+  String _cans2;
+  int _points;
+  Stopwatch watch = new Stopwatch();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,95 +52,288 @@ class _ChallengeQuestionState extends State<ChallengeQuestion> {
                 color: Colors.white,
               ),
               onPressed: () {
-                // Should go to User Account page
-                // UserAccount()
-
+                Navigator.push(context, CupertinoPageRoute(builder: (context) => UserAccount()));
               },
             )
           ],
         ),
 
         body: ListView(
-
-            scrollDirection: Axis.vertical,
-            children: <Widget>[
+          children: <Widget>[
             Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+
+                Text('Solve Questions',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)
+                ),
+                buildQuestionList(questions),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        SizedBox(width: 25,),
+                        Expanded(child:
+                        RaisedButton(
+                          color: Colors.grey[200],
+                          onPressed: (){
+                            setState(() {
+                              _points = 0;
+                            });
+                            watch.stop();
+                            calculatePoints();
+                            print(_points);
+                            print(watch.elapsedMilliseconds);
+                            Navigator.push(context, CupertinoPageRoute(builder: (context) => PerformanceChallenger(points: _points, time: watch.elapsedMilliseconds)));
+                          },
+
+                          child: Text(
+                              "CHALLENGE",style: TextStyle(fontSize: 35,color: Colors.green[700])),
+                        )),
+                        SizedBox(width: 25,),
+
+                      ]
+                  ),
+                ),
+              ],
+            ),
+          ]
+        )
+      );
+  }
+
+  Widget buildQuestionList(apiData) => FutureBuilder(
+      future: apiData,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return Container(
+            height: 300,
+            width: 400,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.all(5),
+                    child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.green)),
+                  ),
+                ),
+              ],
+            )
+        );
+        else if (snapshot.data.length == 0) {
+          return Container(
+              height: 300,
+              width: 400,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      margin: EdgeInsets.all(5),
+                      child: Text("No Questions Here!", style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700
+                      ),
+                        textAlign: TextAlign.center,),
+                    ),
+                  ),
+                ],
+              )
+          );
+        }
+        else {
+          _cans1 = snapshot.data[0].correctanswer;
+          _cans2 = snapshot.data[1].correctanswer;
+          return Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-
-              Text('Solve Questions',
-                textAlign: TextAlign.center, 
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: ListTile(
+                  leading: Text('Q1',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600
+                      )
+                  ),
+                  title: Text('${snapshot.data[0].question}',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 20,
+                      )
+                  ),
+                  subtitle: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text('${snapshot.data[0].answer1}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[0].answer1}',
+                          groupValue: _q1ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q1ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[0].answer2}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[0].answer2}',
+                          groupValue: _q1ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q1ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[0].answer3}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[0].answer3}',
+                          groupValue: _q1ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q1ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[0].answer4}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[0].answer4}',
+                          groupValue: _q1ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q1ans = value;
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-
-              Text('Q: You have to make a pizza with d=28cm! What is the total area of the pizza?',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
-
-//              ListTile(
-//                title: const Text('Lafayette'),
-//                leading: Radio(
-//                  value: Answers.a,
-//                  groupValue: _character,
-//                  onChanged: (SingingCharacter value) {
-//                    setState(() { _character = value; });
-//                  },
-//                ),
-//              ),
-//              ListTile(
-//                title: const Text('Thomas Jefferson'),
-//                leading: Radio(
-//                  value: SingingCharacter.jefferson,
-//                  groupValue: _character,
-//                  onChanged: (SingingCharacter value) {
-//                    setState(() { _character = value; });
-//                  },
-//                ),
-//              ),
-//              ListTile(
-//                title: const Text('Lafayette'),
-//                leading: Radio(
-//                  value: SingingCharacter.lafayette,
-//                  groupValue: _character,
-//                  onChanged: (SingingCharacter value) {
-//                    setState(() { _character = value; });
-//                  },
-//                ),
-//              ),
-//              ListTile(
-//                title: const Text('Lafayette'),
-//                leading: Radio(
-//                  value: SingingCharacter.lafayette,
-//                  groupValue: _character,
-//                  onChanged: (SingingCharacter value) {
-//                    setState(() { _character = value; });
-//                  },
-//                ),
-//              ),
-
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    SizedBox(width: 25,),
-                    Expanded(child:
-                    RaisedButton(
-                      color: Colors.grey[200],
-                      onPressed: (){
-                        Navigator.push(context, CupertinoPageRoute(builder: (context) => PerformanceChallenger()));
-                      },
-
-                      child: Text(
-                          "CHALLENGE",style: TextStyle(fontSize: 35,color: Colors.green[700])),
-                    )),
-                    SizedBox(width: 25,),
-        
-                  ]
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: ListTile(
+                  leading: Text('Q2',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600
+                      )
+                  ),
+                  title: Text('${snapshot.data[1].question}',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 20,
+                      )
+                  ),
+                  subtitle: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text('${snapshot.data[1].answer1}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[1].answer1}',
+                          groupValue: _q2ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q2ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[1].answer2}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[1].answer2}',
+                          groupValue: _q2ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q2ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[1].answer3}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[1].answer3}',
+                          groupValue: _q2ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q2ans = value;
+                            });
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('${snapshot.data[1].answer4}'),
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: '${snapshot.data[1].answer4}',
+                          groupValue: _q2ans,
+                          onChanged: (String value) {
+                            setState(() {
+                              _q2ans = value;
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+              ),
             ],
-          ),
-        ])
+          );
+        }
+      }
       );
+
+  void calculatePoints() {
+    if(_q1ans == _cans1) {
+      setState(() {
+        _points += 100;
+      });
+    }
+    if(_q2ans == _cans2)
+    {
+      setState(() {
+        _points += 100;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    questions = Collections().getChallengeQuestions(widget.level, widget.topic);
+    watch.reset();
+    watch.start();
   }
 }
 
